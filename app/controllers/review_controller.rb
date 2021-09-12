57af7e6b0761d2636a21c0ee55c685ee6d051d85
Chi Vinh Le <vinh@vinh.tech>
2021-09-13 01:43:16 +0200

    feat: mvp

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,22 @@
+class ReviewController < ApplicationController
+  def index
+    @reviews = Review.all
+    @rating = @reviews.inject(0.0) { |sum, el| sum + el.rating } / @reviews.size
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      self.index
+      render :index
+    else
+      self.index
+      # TODO: Should go to error page.
+      render :index
+    end
+  end
+  private
+    def review_params
+      params.require(:review).permit(:rating, :body)
+    end
+end
