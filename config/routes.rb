59b8342664a6263b33897a1ab92258d9cefdcf52
Chi Vinh Le <vinh@vinh.tech>
2021-09-13 21:52:05 +0200

    fix: route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "review#index"
-  get "/review", to: "review#index"
+  post "/", to: "review#create"
   get "/2.0/", to: "review2#index"
   post "/2.0/", to: "review2#create"
 
